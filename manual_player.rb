02c298ee805554084f8aa9d90b5bd0fdfd043661
Marc Noirot <marc.noirot@gmail.com>
2014-04-23 11:19:06 +0200

    Simplified example.

--- manual_player.rb
+++ manual_player.rb
@@ -1,15 +1,11 @@
+$stdout.sync = true
 len = ARGV[0].to_i
 
-words = File.read("wordlist.txt").split("\n")
-words.select! { |w| w.length == len }
+words = File.read("wordlist.txt").split("\n").select { |w| w.length == len }
 
 len.times do
-  line = STDIN.gets.chomp
-  
+  line = $stdin.gets.chomp
   words.select! { |w| !Regexp.new(line).match(w).nil? }
-
   puts words.sample
-  STDOUT.flush
-
-  line = STDIN.gets.chomp
+  line = $stdin.gets.chomp
 end
