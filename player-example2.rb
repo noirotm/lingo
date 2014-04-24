74aa487b7100bae7bb68b2cc725b0bbf5ac4a291
Marc Noirot <marc.noirot@gmail.com>
2014-04-24 16:00:15 +0200

    Finished smarter player.

--- player-example2.rb
+++ player-example2.rb
@@ -3,9 +3,24 @@ len = ARGV[0].to_i
 
 words = File.read("wordlist.txt").split("\n").select { |w| w.length == len }
 
+last_word = nil
+
 len.times do
-  line = $stdin.gets.chomp
-  words.select! { |w| !Regexp.new(line).match(w).nil? }
-  puts words.sample
-  line = $stdin.gets.chomp
+  mask = $stdin.gets.chomp
+ 
+  last_word = words.sample
+  puts last_word
+  
+  errors = $stdin.gets.chomp
+  
+  errors.split('').each_with_index do |c, i|
+    # exclude words not matching goog guesses
+    words.reject! { |w| c == 'O' && w[i] != last_word[i] }
+  
+    # exclude words not containing misplaced letters
+    words.reject! { |w| c == '?' && w.index(last_word[i]).nil? }
+    
+    # exclude words containing wrong letters
+    words.reject! { |w| c == 'X' && !w.index(last_word[i]).nil? }
+  end
 end
