e9996216a0d51646dabb0bf3f8182895c115aa6b
Marc Noirot <marc.noirot@gmail.com>
2014-06-17 00:09:33 +0200

    Switched the pit back to 100 rounds.

--- pit.rb
+++ pit.rb
@@ -2,7 +2,7 @@ require 'rbconfig'
 
 PLAYERS = File.read("players.txt").split("\n")
 LENGTHS = 4..13
-ROUNDS = 10
+ROUNDS = 100
 
 is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
 GAME_ENGINE = is_windows ? 'lingo.exe' : './lingo'
